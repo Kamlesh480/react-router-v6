22a3a5d8c3bf7076f11db580bb95f304a7abe211
Kamlesh Chhipa <kamleshchhipa@kamleshchhipa.local>
2023-05-01 05:45:52 +0530

    BrowserRouter

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,26 @@
+import { Link, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { BookList } from "./pages/BookList";
+
 function App() {
-  return <h1>Hey</h1>;
+  return (
+    <>
+      <nav>
+        <ui>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/books">Books</Link>
+          </li>
+        </ui>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/books" element={<BookList />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
