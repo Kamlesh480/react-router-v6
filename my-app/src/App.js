eea9dae41e24113dc2a5850c3e56b0662a83a392
Kamlesh Chhipa <kamleshchhipa@kamleshchhipa.local>
2023-05-01 07:46:32 +0530

    useRoutes hook

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,25 @@
-import { Link, Route, Routes } from "react-router-dom";
+import { Link, Route, Routes, useRoutes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { BookList } from "./pages/BookList";
 import { Book } from "./pages/Book";
+import { About } from "./pages/About";
+import { Contact } from "./pages/Contact";
 import { NewBook } from "./pages/NewBook";
 import { NotFound } from "./pages/NotFound";
 import { BookLayout } from "./BookLayout";
 import { BookRoutes } from "./BookRoutes";
 
 function App() {
+  let element = useRoutes([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "*",
+      element: <NotFound />,
+    },
+  ]);
   return (
     <>
       <nav>
@@ -20,11 +32,12 @@ function App() {
           </li>
         </ui>
       </nav>
-      <Routes>
+      {element}
+      {/* <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/books/*" element={<BookRoutes />} />
         <Route path="*" element={<NotFound />} />
-      </Routes>
+      </Routes> */}
     </>
   );
 }
